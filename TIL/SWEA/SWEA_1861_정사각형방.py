e1b922415af5eb73146f97c651304474c6d1dcd0
mj <kmjk1117@gmail.com>
2024-08-05 17:49:16 +0900

    20240805 TIL

---
+++ TIL/SWEA/SWEA_1861_정사각형방.py
@@ -0,0 +1,39 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+'''
+00 01
+10 11
+'''
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+tempSum = 0 
+tempList = []
+def recur(y, x, start):
+    global tempSum
+    tempSum += 1
+    visited[y][x] = True
+    tempList.append((tempSum, start))
+    for i in range(4):
+        nowX = x + dx[i]
+        nowY = y + dy[i]
+        if 0 <= nowX < n and 0 <= nowY < n and arr[nowY][nowX] == arr[y][x] +1 and visited[nowY][nowX] == False:
+            recur(nowY, nowX, start)
+            visited[nowY][nowX] = False
+
+for y in range(n):
+    for x in range(n):
+        recur(y, x, arr[y][x])
+        tempSum = 0
+        visited[y][x] = False
+
+tempList.sort()
+print(*tempList)
+
+maxRoomCount = tempList[-1][0]
+start = tempList[-1][1]
+
+for roomCount, roomNum in tempList:
+    if roomCount == maxRoomCount and start > roomNum:
+        start = roomNum
+print(start, maxRoomCount)

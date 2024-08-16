90f9c700a5800bbb6b5c625f6ecba02d0cb14fde
sonnbeom <dhsmf4245@gmail.com>
2024-08-16 17:35:21 +0900

    20055

--- TIL/SWEA/July_2nd/백준_컨베이어_벨트 위의 로봇.py
+++ TIL/SWEA/July_2nd/백준_컨베이어_벨트 위의 로봇.py
@@ -2,39 +2,28 @@ from collections import deque
 
 n, k = map(int, input().split())
 belt = deque(list(map(int, input().split())))
-robo = [-1 for _ in range(2*n)]
-robot = deque(robo)
+robot = deque([False for _ in range(n)])
 ans = 0
-zero = 0
-w = 0
-while k > zero:
+
+
+while True:
     ans += 1
     belt.rotate(1)
     robot.rotate(1)
-    for i in range(w-1, -1, -1):
-        t = i+2 % (2*n)
-        if robot[t] == -1 and belt[t] != 0: #로봇이 없고 벨트 값이 0이 아니라면
-            robot[i] = -1
-            robot[t] = t #로봇위치 1칸 이동
-            belt[t] -= 1 # 벨트 값 감소
-            if belt[t] == 0: # 벨트 값이 0이라면 ans 증가
-                k -= 1
-                zero += 1
-                if k == zero:
-                    break
-            if robot[t] == n-1: # 만약 내리는 위치라면 없애
-                robot[n-1] = -1
-                w -= 1
-                # robot.remove(robot[i])
-    if belt[0] != 0 and robot[0] == -1:
-        belt[0] -= 1
-        w += 1
-        if belt[0] == 0:
-            k -= 1
-            zero += 1
-            if k == zero:
-                break
-        robot[0] = 0
 
+    robot[n-1] = False
+
+    for i in range(n-2, -1, -1):
+        if belt[i+1] > 0 and robot[i] == True and robot[i+1] == False:
+            robot[i] = False
+            robot[i+1] = True
+            belt[i+1] -= 1
+    robot[n-1] = False
+
+    if belt[0] > 0:
+        belt[0] -= 1
+        robot[0] = True
+    if belt.count(0) >= k:
+        break
 
 print(ans)

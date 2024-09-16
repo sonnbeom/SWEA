6b9ce707ccb7898e9c4bb76dcdbd1df91507cdca
sonny <dhsmf4245@gmail.com>
2024-09-16 10:54:06 +0900

    2024 09 16 TIL

--- TIL/September/3rd_week/백준_1766_문제집.py
+++ TIL/September/3rd_week/백준_1766_문제집.py
@@ -8,3 +8,40 @@
 3. 방문 배열의 갯수의 0이 아니라면
 if문을 돌며 방문 배열 idx 출력
 '''
+
+n, m = map(int, input().split())
+visited = [False for _ in range(n+1)]
+visited[0] = True
+
+arr = [[] for _ in  range(n+1)]
+
+way = []
+
+for i in range(m):
+    a, b = map(int, input().split())
+    way.append((a, b))
+
+for a, b in way:
+    if len(arr[a]) == 0:
+        arr[a].append(b)
+    elif len(arr[a]) >= 1:
+        t = min(arr[a][0], b)
+        arr[a][0] = b
+ans = []
+tmp_list = []
+for i in range(1, n+1):
+    if len(arr[i]) == 1 and not visited[arr[i]]:
+        ans.append(i)
+        ans.append(arr[i][0])
+        visited[i] = True
+        visited[arr[i][0]] = True
+
+    else:
+        tmp_list.append(i)
+
+if visited.count(False) > 0:
+    if len(tmp_list) > 0:
+        for num in tmp_list:
+            if not visited[num]:
+                ans.append(num)
+print(*ans)

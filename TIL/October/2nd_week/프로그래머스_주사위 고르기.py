b2db76e071ae10ab00d8b2dfac5a9bb87b3453df
sonnbeom <dhsmf4245@gmail.com>
2024-10-09 16:21:13 +0900

    2024 10 09 TIL

--- TIL/October/2nd_week/프로그래머스_주사위 고르기.py
+++ TIL/October/2nd_week/프로그래머스_주사위 고르기.py
@@ -13,25 +13,37 @@ def binary_search(now, other_case):
         else:
             high = mid - 1
     return low
+'''
+0 2 4 6 에서 1의 위치는 어디일까?
+1. 
+mid = 1
+2보다 작다
+high = 0
+2. mid = 0
+0보다 크다.
+low = 0 + 1
+low =1
+3. low가 더 크다.
+low 리턴
+'''
 
 
 def dfs(case, dice, depth, now, res):
     # 깊이(더한 주사위들의 갯수)가 case의 길이와 같다면 리턴 case의 길이: 각 경우의 수
-    if len(case) == idx:
+    if len(case) == depth:
         res.append(now)
         return
-    dice_idx = case[idx]
+    dice_idx = case[depth]
     for d in dice[dice_idx]:
         dfs(case, dice, depth + 1, now + d, res)
 
 
 def solution(dice):
     answer = []
-    half = len(dice) // 2
     li = [i for i in range(len(dice))]
+    half = len(dice) // 2
     # 주사위를 선택할 수 있는 조합의 경우를 찾기
-    cases = list(itertools.combintations(li, 3))
-
+    cases = list(itertools.combinations(li, half))
     sum_cases = {}
 
     for idx, case in enumerate(cases):
@@ -48,10 +60,19 @@ def solution(dice):
         # 나
         my_case = value
         # 상대방 반대편이므로
-        ohter_case = sum_cases[case_len - 1 - idx]
+        other_case = sum_cases[case_len - 1 - idx]
 
         temp_sum = 0
         for num in my_case:
-            temp_sum += binary_search(num, ohter_case)
+            temp_sum += binary_search(num, other_case)
 
+        if temp_sum > max_sum:
+            max_sum = temp_sum
+            # itertools.combinations는 튜플을 반환한다. (0,1), (1,2)
+            temp_best_case = list(cases[idx])
+            temp_best_case.sort()
+            answer = temp_best_case
+    # 인덱스이므로 +1
+    for i in range(len(answer)):
+        answer[i] += 1
     return answer

c929548553e57093f62363398c8da51ea4f4b111
soony <dhsmf4245@gmail.com>
2024-12-04 12:55:59 +0900

    2024 12 04 TIL

---
+++ TIL/December/BJ_12865.py
@@ -0,0 +1,24 @@
+def func(weight, value):
+    for i in range(len(dp)):
+        idx = i + weight
+        if idx < len(dp):
+            dp[idx] = max(dp[idx], dp[i] + value)
+        else:
+            return
+
+n, k = map(int, input().split())
+dp = [0 for _ in range(k+1)]
+
+bag = []
+for _ in range(n):
+    w, v = map(int, input().split())
+    bag.append(((w,v)))
+
+bag.sort()
+for w, v in bag:
+    func(w, v)
+    print(f'w = {w} v = {v}')
+    print(dp)
+
+print(dp)
+print(max(dp))

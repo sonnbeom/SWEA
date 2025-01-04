b1d33b566a3d1ca22a7cbe038844fd127411e0ae
soony <dhsmf4245@gmail.com>
2025-01-04 18:05:54 +0900

    TIL

--- 2025/1월/첫째주/BOJ_17873_새로운게임2.py
+++ 2025/1월/첫째주/BOJ_17873_새로운게임2.py
@@ -1,10 +1,42 @@
 # 0은 흰색, 1은 빨간색, 2는 파란색
-from SWEA.TIL.SWEA.Past.SWEA_4408_자기방으로돌아가기 import small
+# 아예 무빙이 없는 경우도 불가능이다.
+
+def get_reverse_num(idx):
+    if idx == 0:
+        return 1
+    elif idx == 1:
+        return 0
+    elif idx == 2:
+        return 3
+    else:
+        return 2
+def check_range(ny, nx):
+    if not(0 <= nx < n and 0 <= ny < n):
+        return False
+    else:
+        return True
+def move(y, x, idx, horse):
+    num = horse[idx]
+    ny = y + dy[num]
+    nx = x + dx[num]
+    if not check_range(ny, nx):
+        num = get_reverse_num(num)
+        ny = y + dy[num]
+        nx = x + dx[num]
+        if not check_range(ny, nx):
+            return
+    elif chess[ny][nx] == 0:
 
 
-def move(y, x):
+
+
+def func(y, x):
     horse = horses[y][x]
-    small_num = min(horse)
+    if len(horse) >= 4:
+        return False
+    small = min(horse)
+    idx = horse.index(small)
+    move(y, x, idx, horse)
     pass
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -18,13 +50,15 @@ for _ in range(k):
     y, x, d = map(int, input().split())
     horses[y-1][x-1].append(d-1)
 cnt = 0
+checked = [False for _ in range(k)]
 while True:
     cnt += 1
     for y in range(n):
         for x in range(n):
             if horses:
-                move(y, x)
-
+                res = func(y, x)
+                if not res:
+                    break
 
 
 

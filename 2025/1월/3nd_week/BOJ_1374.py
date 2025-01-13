a37e282cdcbc36f7ebcddedca5cd579059a5249f
sonnbeom <dhsmf4245@gmail.com>
2025-01-13 15:24:31 +0900

    TIL

--- 2025/1월/3nd_week/BOJ_1374.py
+++ 2025/1월/3nd_week/BOJ_1374.py
@@ -1,1 +1,14 @@
+from collections import deque
 n = int(input())
+
+pools = []
+
+q = deque()
+for _ in range(n):
+    num, s, e = map(int, input().split())
+    temp = [s, num, e]
+    q.append(temp)
+arr.sort()
+
+
+
